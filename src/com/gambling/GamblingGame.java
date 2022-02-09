9a0738619ed29c4c511d35c63baf2eaa2115fad6
17harshu <harshalanandas17@gmail.com>
2022-02-09 12:38:22 +0530

    added: win or lose

--- src/com/gambling/GamblingGame.java
+++ src/com/gambling/GamblingGame.java
@@ -1,14 +1,23 @@
 package com.gambling;
 
 public class GamblingGame {
+	static int stake = 100;
+	static int bet = 1;
 	
-	public static void startGambling() {
-		int stake = 100;
-		int bet = 1;
+	public static void winLose() {
+		double ran = Math.floor((Math.random()*10)%2);
+		System.out.println("Random Value: " + ran);
+        if(ran == bet)
+            stake++;
+        else
+            stake--;
+        System.out.println("Stake value: " + stake);
 	}
+	
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulator");
-		startGambling();
+		
+		winLose();
 	}
 }
