d11dd2c2fd3a7464a92df67265b0a02826549cff
17harshu <harshalanandas17@gmail.com>
2022-02-09 12:04:32 +0530

    added: initialized stake and bet value

--- src/com/gambling/GamblingGame.java
+++ src/com/gambling/GamblingGame.java
@@ -1,7 +1,14 @@
 package com.gambling;
 
 public class GamblingGame {
+	
+	public static void startGambling() {
+		int stake = 100;
+		int bet = 1;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulator");
+		startGambling();
 	}
 }
